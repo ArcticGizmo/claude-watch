5a539dbb0eaf3e2bd6c67323297e4e7a30ae37c6
Jonathan Howell <jon.howell314@gmail.com>
2026-06-21 15:59:46 +0800

    Fix copy to clipboard

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,18 @@
 using ClaudeWatch;
 using Velopack;
 
-VelopackApp.Build().Run();
+internal static class Program
+{
+    // Must be STA: the WinForms clipboard (and other OLE-backed features) throw on an MTA thread,
+    // and top-level statements don't emit [STAThread] on the generated entry point.
+    [STAThread]
+    private static void Main()
+    {
+        VelopackApp.Build().Run();
 
-Application.EnableVisualStyles();
-Application.SetCompatibleTextRenderingDefault(false);
-Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
-Application.Run(new OverlayApplicationContext());
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+        Application.Run(new OverlayApplicationContext());
+    }
+}
